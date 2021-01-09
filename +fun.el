df1ccdecc03ac8356e1c8a6295d9ec908be62f9e
Boris Buliga <boris@d12frosted.io>
2021-01-09 14:27:30 +0200

    (dev) `+repeat-fn' helper

---
+++ +fun.el
@@ -0,0 +1,47 @@
+;;; +fun.el --- Function extensions -*- lexical-binding: t; -*-
+;;
+;; Copyright (c) 2021 Boris Buliga
+;;
+;; Author: Boris Buliga <boris@d12frosted.io>
+;; Maintainer: Boris Buliga <boris@d12frosted.io>
+;;
+;; Created: 09 Jan 2021
+;;
+;; URL: https://github.com/d12frosted/vino.el
+;;
+;; License: GPLv3
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; Commentary:
+;;
+;; Extra functions one might hate to use.
+;;
+;;; Code:
+
+(defun +repeat-fn (fn filter &rest args)
+  "Repeat FN and collect it's results until `C-g` is used.
+
+Repeat cycle stops when `C-g` is used or FILTER returns nil.
+
+If FILTER is nil, it does not affect repeat cycle.
+
+If FILTER returns nil, the computed value is not added to result.
+
+ARGS are passed to FN."
+  (let (result
+        value
+        (continue t)
+        (inhibit-quit t))
+    (with-local-quit
+      (while continue
+        (setq value (apply fn args))
+        (if (and filter
+                 (null (funcall filter value)))
+            (setq continue nil)
+          (setq result (cons value result)))))
+    (setq quit-flag nil)
+    (seq-reverse result)))
+
+(provide '+fun)
+;;; +fun.el ends here
---
+++ test/+fun-test.el
@@ -0,0 +1,33 @@
+;;; +fun-test.el --- Test `+fun' module -*- lexical-binding: t; -*-
+;;
+;; Copyright (c) 2021 Boris Buliga
+;;
+;; Author: Boris Buliga <boris@d12frosted.io>
+;; Maintainer: Boris Buliga <boris@d12frosted.io>
+;;
+;; Created: 09 Jan 2021
+;;
+;; URL: https://github.com/d12frosted/vino.el
+;;
+;; License: GPLv3
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; Commentary:
+;;
+;; Test `+fun' module/
+;;
+;;; Code:
+
+(require 'buttercup)
+(require '+fun)
+
+(describe "+repeat-fn"
+  (it "repeats a function until filter returns nil"
+    (let ((n 0))
+      (expect (+repeat-fn (lambda () (setq n (+ 1 n)))
+                          (lambda (v) (< v 5)))
+              :to-equal '(1 2 3 4)))))
+
+(provide '+fun-test)
+;;; +fun-test.el ends here
