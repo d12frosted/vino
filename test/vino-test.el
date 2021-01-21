1ad0eec3949a474eae986d56c4cc817136142331
Boris Buliga <boris@d12frosted.io>
2021-01-21 10:53:15 +0200

    (release) v0.1

--- test/vino-test.el
+++ test/vino-test.el
@@ -1,15 +1,29 @@
 ;;; vino-test.el --- Test `vino' module -*- lexical-binding: t; -*-
 ;;
-;; Copyright (c) 2021 Boris Buliga
+;; Copyright (c) 2020-2021 Boris Buliga
 ;;
 ;; Author: Boris Buliga <boris@d12frosted.io>
 ;; Maintainer: Boris Buliga <boris@d12frosted.io>
 ;;
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License as
+;; published by the Free Software Foundation, either version 3 of the
+;; License, or (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+;; General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program. If not, see
+;; <http://www.gnu.org/licenses/>.
+;;
+;; License: GPLv3
+;;
 ;; Created: 09 Jan 2021
 ;;
 ;; URL: https://github.com/d12frosted/vino
-;;
-;; License: GPLv3
 ;;
 ;; This file is not part of GNU Emacs.
 ;;
--- vino.el
+++ vino.el
@@ -1,17 +1,31 @@
 ;;; vino.el --- Cellar tracking with vulpea    -*- lexical-binding: t; -*-
 ;;
-;; Copyright (c) 2021 Boris Buliga
+;; Copyright (c) 2020-2021 Boris Buliga
 ;;
 ;; Author: Boris Buliga <boris@d12frosted.io>
 ;; Maintainer: Boris Buliga <boris@d12frosted.io>
-;; Package-Version: 1.0
+;; Version: 0.1
 ;; Package-Requires: ((emacs "27.1"))
+;;
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License as
+;; published by the Free Software Foundation, either version 3 of the
+;; License, or (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+;; General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program. If not, see
+;; <http://www.gnu.org/licenses/>.
+;;
+;; License: GPLv3
 ;;
 ;; Created: 09 Jan 2021
 ;;
 ;; URL: https://github.com/d12frosted/vino
-;;
-;; License: GPLv3
 ;;
 ;; This file is not part of GNU Emacs.
 ;;
