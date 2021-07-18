cc51de42d32281a506abbd797cf57962683127dc
Boris Buliga <boris@d12frosted.io>
2021-07-18 09:44:52 +0300

    release v0.3.0

--- vino.el
+++ vino.el
@@ -4,8 +4,8 @@
 ;;
 ;; Author: Boris Buliga <boris@d12frosted.io>
 ;; Maintainer: Boris Buliga <boris@d12frosted.io>
-;; Version: 0.1.2
-;; Package-Requires: ((emacs "27.1") (vulpea "0.1") (org-roam "1.2.3"))
+;; Version: 0.3.0
+;; Package-Requires: ((emacs "27.1") (vulpea "0.1") (org-roam "2.0.0"))
 ;;
 ;; This program is free software; you can redistribute it and/or
 ;; modify it under the terms of the GNU General Public License as
